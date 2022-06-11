6fdbd659279ba3a5f88cc719d3b8cf7f2b53311b
Carolina Brandão <up202004461@edu.fc.up.pt>
2022-06-11 17:01:48 +0100

    Exercicio 2

--- Pratica9/Exercicio 2/Date.cpp
+++ Pratica9/Exercicio 2/Date.cpp
@@ -6,5 +6,5 @@ Date::Date(int year, int month, int day) : year_(year), month_(month), day_(day)
 
 bool Date::is_before(const Date &date) const
 {
-    return (year_*10000+month_*100+day_< date.getYear()*10000+date.getMonth()*100+date.getDay());
+    return (year_ * 10000 + month_ * 100 + day_ < date.getYear() * 10000 + date.getMonth() * 100 + date.getDay());
 }
